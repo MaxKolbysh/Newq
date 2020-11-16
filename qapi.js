df2817043a2e819fb40e113899ff0d2563600f59
MaxKolbysh <36901647+MaxKolbysh@users.noreply.github.com>
2020-11-16 13:08:00 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb245bd8996cc001edce839",
+				"5fb245bd8996cc001edce83b",
+				"5fb2468ab83a2f001f2dec8d"
+			]
+		},
+		"5fb245bd8996cc001edce839": {
+			"id": "5fb245bd8996cc001edce839",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb245bd8996cc001edce83b": {
+			"id": "5fb245bd8996cc001edce83b",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5fb2468ab83a2f001f2dec8d": {
+			"id": "5fb2468ab83a2f001f2dec8d",
+			"pageUrl": "about",
+			"name": "about"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
